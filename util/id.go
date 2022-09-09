8d77d746d01d6f8cc4a8991b09a3afc18de77e92
Nightmare <nightzhuxy@hotmail.com>
2022-09-09 17:19:02 +0800

    增加去除 '-' 的uuid生成

--- util/id.go
+++ util/id.go
@@ -11,25 +11,36 @@ var (
 	random1 = rand.Reader
 )
 
-// 生成短id
+// 生成短id(22个字符)
 func ShortId() string {
 	uuid := uuidR16()
 	return base64.RawURLEncoding.EncodeToString(uuid[:])
 }
 
+func Uuid32() string {
+	uuid := uuidR16()
+	target := [32]byte{}
+
+	hex.Encode(target[:], uuid[:])
+
+	return string(target[:])
+}
+
+// 生成UUID (以"-"分隔的 UUID.V4)
 func Uuid() string {
 	uuid := uuidR16()
 	target := [36]byte{}
 
 	hex.Encode(target[:], uuid[:4])
-	target[8] = '-'
 	hex.Encode(target[9:13], uuid[4:6])
-	target[13] = '-'
 	hex.Encode(target[14:18], uuid[6:8])
-	target[18] = '-'
 	hex.Encode(target[19:23], uuid[8:10])
-	target[23] = '-'
 	hex.Encode(target[24:], uuid[10:])
+
+	target[8] = '-'
+	target[13] = '-'
+	target[18] = '-'
+	target[23] = '-'
 
 	return string(target[:])
 }
