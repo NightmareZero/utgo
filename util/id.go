10b28d12d9846bd1115f3a7e918abf5e1b70470b
Nightmare <nightzhuxy@hotmail.com>
2022-09-14 11:40:28 +0800

    添加uuid.V1

--- util/id.go
+++ util/id.go
@@ -3,6 +3,7 @@ package util
 import (
 	"crypto/rand"
 	"encoding/base64"
+	"encoding/binary"
 	"encoding/hex"
 	"io"
 )
@@ -11,46 +12,109 @@ var (
 	random1 = rand.Reader
 )
 
+type uuid [16]byte
+
 // 生成短id(22个字符)
-func Uuid22() string {
-	uuid := uuidR16()
+func UuidV4_22() string {
+	uuid := uuidV4()
+	return base64.RawURLEncoding.EncodeToString(uuid[:])
+}
+
+func UuidV1_22() string {
+	uuid := uuidV1()
 	return base64.RawURLEncoding.EncodeToString(uuid[:])
 }
 
 // 生成不带 '-' 的uuid
-func Uuid32() string {
-	uuid := uuidR16()
+func UuidV4_32() string {
+	uuid := uuidV4()
 	target := [32]byte{}
 
-	hex.Encode(target[:], uuid[:])
+	encodeToString(uuid[:], target[:], false)
+
+	return string(target[:])
+}
+
+// 生成不带 '-' 的uuid
+func UuidV1_32() string {
+	uuid := uuidV1()
+	target := [32]byte{}
+
+	encodeToString(uuid[:], target[:], false)
+
+	return string(target[:])
+}
+
+// 生成UUID (以"-"分隔的 UUID.V1)
+func UuidV1() string {
+	uuid := uuidV1()
+	target := [36]byte{}
+
+	encodeToString(uuid[:], target[:], true)
 
 	return string(target[:])
 }
 
 // 生成UUID (以"-"分隔的 UUID.V4)
-func Uuid() string {
-	uuid := uuidR16()
+func UuidV4() string {
+	uuid := uuidV4()
 	target := [36]byte{}
 
-	hex.Encode(target[:], uuid[:4])
-	hex.Encode(target[9:13], uuid[4:6])
-	hex.Encode(target[14:18], uuid[6:8])
-	hex.Encode(target[19:23], uuid[8:10])
-	hex.Encode(target[24:], uuid[10:])
-
-	target[8] = '-'
-	target[13] = '-'
-	target[18] = '-'
-	target[23] = '-'
+	encodeToString(uuid[:], target[:], true)
 
 	return string(target[:])
 }
 
-func uuidR16() (r16 [16]byte) {
+// 生成uuidV4需要的16位byte随机数
+func uuidV4() (r16 uuid) {
 	io.ReadFull(random1, r16[:])
 
 	r16[6] = (r16[6] & 0x0f) | 0x40 // Version 4
 	r16[8] = (r16[8] & 0x3f) | 0x80 // Variant is 10
 
 	return
+}
+
+// 生成uuidV1需要的16位byte数
+func uuidV1() (mu uuid) {
+	now, seq, err := getTime()
+	if err != nil {
+		return
+	}
+
+	timeLow := uint32(now & 0xffffffff)
+	timeMid := uint16((now >> 32) & 0xffff)
+	timeHi := uint16((now >> 48) & 0x0fff)
+	timeHi |= 0x1000 // Version 1
+
+	binary.BigEndian.PutUint32(mu[0:], timeLow)
+	binary.BigEndian.PutUint16(mu[4:], timeMid)
+	binary.BigEndian.PutUint16(mu[6:], timeHi)
+	binary.BigEndian.PutUint16(mu[8:], seq)
+
+	setNodeInterface(mu[10:])
+
+	return
+}
+
+func encodeToString(uuid []byte, buf []byte, hyphen bool) {
+	if hyphen {
+		hex.Encode(buf[:], uuid[:4])
+		hex.Encode(buf[9:13], uuid[4:6])
+		hex.Encode(buf[14:18], uuid[6:8])
+		hex.Encode(buf[19:23], uuid[8:10])
+		hex.Encode(buf[24:], uuid[10:])
+
+		buf[8] = '-'
+		buf[13] = '-'
+		buf[18] = '-'
+		buf[23] = '-'
+	} else {
+		hex.Encode(buf[:], uuid[:])
+	}
+}
+
+// 设置 uuidV1 需要的网卡名称
+func SetV1InterfaceName(name string) {
+	infName = name
 }
---
+++ util/id_ut.go
@@ -0,0 +1,95 @@
+package util
+
+import (
+	"io"
+	"net"
+	"sync"
+	"time"
+)
+
+var (
+	infName string // 网卡名称
+	infAddr []byte // 网卡地址
+
+	lasttime uint64     // last time we returned
+	clockSeq uint16     // clock sequence for this run
+	timeMu   sync.Mutex // uuidV1时间锁
+)
+
+// 想指定的位置写入网卡地址
+func setNodeInterface(macId []byte) bool {
+	if infAddr != nil {
+		copy(macId, infAddr)
+	}
+
+	iname, addr := getHardwareInterface() // null implementation for js
+	if iname != "" && addr != nil {
+		infName = iname
+		copy(infAddr, addr)
+		copy(macId, addr)
+		return true
+	}
+
+	// 如果始终未获取到
+	if addr == nil {
+		infName = "_"
+		io.ReadFull(random1, infAddr)
+		copy(macId, infAddr)
+		return true
+	}
+	return false
+}
+
+// 获取 uuid v1 需要的网卡mac地址
+func getHardwareInterface() (string, []byte) {
+	netInterfaces, err := net.Interfaces()
+	if err != nil {
+		return "", nil
+	}
+
+	for _, ifs := range netInterfaces {
+		if len(ifs.HardwareAddr) >= 6 && (infName == "" || infName == ifs.Name) {
+			return ifs.Name, ifs.HardwareAddr
+		}
+	}
+	return "", nil
+}
+
+const (
+	lillian    = 2299160          // Julian day of 15 Oct 1582
+	unix       = 2440587          // Julian day of 1 Jan 1970
+	epoch      = unix - lillian   // Days between epochs
+	g1582      = epoch * 86400    // seconds between epochs
+	g1582ns100 = g1582 * 10000000 // 100s of a nanoseconds between epochs
+)
+
+func getTime() (int64, uint16, error) {
+	t := time.Now()
+
+	// If we don't have a clock sequence already, set one.
+	if clockSeq == 0 {
+		setClockSequence(-1)
+	}
+	now := uint64(t.UnixNano()/100) + g1582ns100
+
+	// If time has gone backwards with this clock sequence then we
+	// increment the clock sequence
+	if now <= lasttime {
+		clockSeq = ((clockSeq + 1) & 0x3fff) | 0x8000
+	}
+	lasttime = now
+	return int64(now), clockSeq, nil
+}
+
+func setClockSequence(seq int) {
+	if seq == -1 {
+		var b [2]byte
+		io.ReadFull(random1, b[:]) // clock sequence
+		seq = int(b[0])<<8 | int(b[1])
+	}
+	oldSeq := clockSeq
+	clockSeq = uint16(seq&0x3fff) | 0x8000 // Set our variant
+	if oldSeq != clockSeq {
+		lasttime = 0
+	}
+}
