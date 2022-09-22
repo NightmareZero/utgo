e24527d0d40cf51a83b95c15784ad29db21fee49
Nightmare <nightzhuxy@hotmail.com>
2022-09-22 16:18:44 +0800

    完成formdata支持，支持文件上传和内容

--- hsrv/reqs.go
+++ hsrv/reqs.go
@@ -4,7 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"mime/multipart"
 	"net/http"
+	"strings"
 )
 
 type Request struct {
@@ -64,4 +66,55 @@ func (r *Response) Json(target any, statusCode int) error {
 	r.Header().Add("Content-Type", "application/json; charset=utf-8")
 	_, err = r.Write([]byte(b))
 	return err
+}
+
+func (r *Request) FormData(writer func(string, io.Reader, error), multiThread bool) (map[string][]string, error) {
+	contentType := r.Header.Get("content-type")
+	contentLen := r.ContentLength
+
+	var formValue map[string][]string = make(map[string][]string)
+	if !strings.Contains(contentType, "multipart/form-data") {
+		return formValue, fmt.Errorf("content-type must be multipart/form-data")
+	}
+
+	var err error
+	if contentLen < 4*1024*1024 {
+		err = r.ParseMultipartForm(4 * 1024 * 1024)
+	} else {
+		err = r.ParseMultipartForm(16 * 1024 * 1024)
+	}
+	if err != nil {
+		return formValue, fmt.Errorf("failure on parse files")
+	}
+
+	if len(r.MultipartForm.File) == 0 {
+		return formValue, fmt.Errorf("not have any file")
+	}
+
+	for k, v := range r.MultipartForm.Value {
+		formValue[k] = v
+	}
+
+	for name, content := range r.MultipartForm.File {
+		if multiThread {
+			go uploadFilesWorker(name, content, writer)
+		} else {
+			uploadFilesWorker(name, content, writer)
+		}
+
+	}
+	return formValue, nil
+}
+
+func uploadFilesWorker(name string, cc []*multipart.FileHeader,
+	writer func(string, io.Reader, error)) {
+	for _, fh := range cc {
+		f, err2 := fh.Open()
+		if err2 != nil {
+			writer("", nil, err2)
+		}
+		writer(fh.Filename, f, nil)
+		f.Close()
+	}
+
 }
