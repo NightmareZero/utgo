6beae0f77aa53442a124d06d6fa817568a58a0ea
Nightmare <nightzhuxy@hotmail.com>
2022-09-23 15:48:38 +0800

    添加文件下载

--- hsrv/reqs.go
+++ hsrv/reqs.go
@@ -6,48 +6,18 @@ import (
 	"io"
 	"mime/multipart"
 	"net/http"
+	"strconv"
 	"strings"
 )
-
-type Request struct {
-	*http.Request
-}
-
-type Response struct {
-	http.ResponseWriter
-}
 
 type RequestHandler func(Response, Request)
 
 type ErrorHandler func(Response, Request, error)
 
-func (r *Request) Text() (string, error) {
-	defer r.Body.Close()
-	b, err := io.ReadAll(r.Body)
-	if err != nil {
-		return "", fmt.Errorf("error on read request body, %w", err)
-	}
-	return string(b), nil
-}
+// Response=========================================================
 
-func (r *Request) Json(target any) error {
-	defer r.Body.Close()
-	b, err := io.ReadAll(r.Body)
-	if err != nil {
-		return fmt.Errorf("error on read request body, %w", err)
-	}
-
-	err = json.Unmarshal(b, target)
-	if err != nil {
-		return fmt.Errorf("error on unmarshal json, %w", err)
-	}
-	return nil
-}
-
-func (r *Request) Stream(wr io.Writer) (err error) {
-	defer r.Body.Close()
-	_, err = io.Copy(wr, r.Body)
-	return
+type Response struct {
+	http.ResponseWriter
 }
 
 func (r *Response) Text(txt string, statusCode int) (err error) {
@@ -68,53 +38,123 @@ func (r *Response) Json(target any, statusCode int) error {
 	return err
 }
 
-func (r *Request) FormData(writer func(string, io.Reader, error), multiThread bool) (map[string][]string, error) {
+func (r *Response) File(input io.Reader, size int64, name string) (int64, error) {
+	r.WriteHeader(http.StatusOK)
+	r.Header().Set("Content-Disposition", "attachment; filename="+name)
+	r.Header().Set("Content-Type", "application/octet-stream")
+	r.Header().Set("Content-Length", strconv.FormatInt(size, 10))
+	w, err := io.Copy(r, input)
+	if err != nil {
+		return w, fmt.Errorf("fail on response file, %w", err)
+	}
+
+	return w, nil
+}
+
+// Request=============================================================
+
+type Request struct {
+	*http.Request
+}
+
+func (r *Request) Text() (string, error) {
+	defer r.Body.Close()
+	b, err := io.ReadAll(r.Body)
+	if err != nil {
+		return "", fmt.Errorf("error on read request body, %w", err)
+	}
+	return string(b), nil
+}
+
+func (r *Request) Json(target any) error {
+	defer r.Body.Close()
+	contentType := r.Header.Get("content-type")
+	if !strings.Contains(contentType, "application/json") {
+		return fmt.Errorf("content-type must be application/json")
+	}
+
+	b, err := io.ReadAll(r.Body)
+	if err != nil {
+		return fmt.Errorf("error on read request body, %w", err)
+	}
+
+	err = json.Unmarshal(b, target)
+	if err != nil {
+		return fmt.Errorf("error on unmarshal json, %w", err)
+	}
+	return nil
+}
+
+func (r *Request) Stream(wr io.Writer) (err error) {
+	defer r.Body.Close()
+	_, err = io.Copy(wr, r.Body)
+	return
+}
+
+func (r *Request) FormData() (map[string][]string, error) {
+	defer r.Body.Close()
+	contentType := r.Header.Get("content-type")
+
+	var formValue map[string][]string = make(map[string][]string)
+	if !strings.Contains(contentType, "application/x-www-form-urlencoded") {
+		return formValue, fmt.Errorf("content-type must be application/x-www-form-urlencoded")
+	}
+
+	err := r.ParseForm()
+	if err != nil {
+		return formValue, fmt.Errorf("error on pares form data, %w", err)
+	}
+
+	return r.Form, nil
+}
+
+type MultiForm struct {
+	close bool
+	*multipart.Form
+	r *Request
+}
+
+func (m *MultiForm) Close() {
+	if m.close {
+		m.r.Body.Close()
+		m.close = true
+	}
+}
+
+func (m *MultiForm) ParseFile(writer func(string, io.Reader, error)) {
+	for _, content := range m.File {
+		for _, fh := range content {
+			f, err2 := fh.Open()
+			if err2 != nil {
+				writer("", nil, err2)
+			}
+			writer(fh.Filename, f, nil)
+			f.Close()
+		}
+	}
+	m.Close()
+}
+
+func (r *Request) MultipartForm() (f MultiForm, err error) {
 	contentType := r.Header.Get("content-type")
 	contentLen := r.ContentLength
 
-	var formValue map[string][]string = make(map[string][]string)
 	if !strings.Contains(contentType, "multipart/form-data") {
-		return formValue, fmt.Errorf("content-type must be multipart/form-data")
+		err = fmt.Errorf("content-type must be multipart/form-data")
+		return
 	}
 
-	var err error
+	var err2 error
 	if contentLen < 4*1024*1024 {
-		err = r.ParseMultipartForm(4 * 1024 * 1024)
+		err2 = r.ParseMultipartForm(4 * 1024 * 1024)
 	} else {
-		err = r.ParseMultipartForm(16 * 1024 * 1024)
+		err2 = r.ParseMultipartForm(16 * 1024 * 1024)
 	}
-	if err != nil {
-		return formValue, fmt.Errorf("failure on parse files")
+	if err2 != nil {
+		err = fmt.Errorf("failure on parse files, %w", err2)
+		return
 	}
+	f.Form = r.Request.MultipartForm
 
-	if len(r.MultipartForm.File) == 0 {
-		return formValue, fmt.Errorf("not have any file")
-	}
-
-	for k, v := range r.MultipartForm.Value {
-		formValue[k] = v
-	}
-
-	for name, content := range r.MultipartForm.File {
-		if multiThread {
-			go uploadFilesWorker(name, content, writer)
-		} else {
-			uploadFilesWorker(name, content, writer)
-		}
-
-	}
-	return formValue, nil
-}
-
-func uploadFilesWorker(name string, cc []*multipart.FileHeader,
-	writer func(string, io.Reader, error)) {
-	for _, fh := range cc {
-		f, err2 := fh.Open()
-		if err2 != nil {
-			writer("", nil, err2)
-		}
-		writer(fh.Filename, f, nil)
-		f.Close()
-	}
-
+	return
 }
