611a8d85d9134b8d047270e8b818e3cc711c815d
Nightmare <nightzhuxy@hotmail.com>
2022-10-22 16:08:59 +0800

    修改timecache,允许弹出错误

--- util/timecache.go
+++ util/timecache.go
@@ -8,7 +8,7 @@ import (
 var _ TimeCache[int] = &timeCache[int]{}
 
 type TimeCache[T any] interface {
-	Get() T
+	Get() (T, error)
 	Reset()
 }
 
@@ -17,28 +17,27 @@ type timeCache[T any] struct {
 	lock    sync.Mutex
 	getTime time.Time
 	expire  time.Duration
-	getter  func() T
+	getter  func() (T, error)
+	err     error
 }
 
 // Get implements TimeCache
-func (tc *timeCache[T]) Get() T {
-	if tc.isTimeout(tc.expire) {
+func (tc *timeCache[T]) Get() (T, error) {
+	if tc.isTimeout(tc.expire) || tc.err != nil {
 		tc.lock.Lock()
 		defer tc.lock.Unlock()
-		tc.data = tc.getter()
-		tc.getTime = time.Now()
-		return tc.data
+		tc.reget()
+		return tc.data, tc.err
 	} else if tc.isTimeout(tc.expire / 2) {
 		go func() {
 			b := tc.lock.TryLock()
 			if b {
 				defer tc.lock.Unlock()
-				tc.data = tc.getter()
-				tc.getTime = time.Now()
+				tc.reget()
 			}
 		}()
 	}
-	return tc.data
+	return tc.data, tc.err
 }
 
 // Reset implements TimeCache
@@ -52,7 +51,15 @@ func (tc *timeCache[T]) isTimeout(exp time.Duration) bool {
 	return tc.getTime.Add(tc.expire).Before(time.Now())
 }
 
-func NewTimeCache[T any](expire time.Duration, getter func() T) *timeCache[T] {
+func (tc *timeCache[T]) reget() {
+	tc.data, tc.err = tc.getter()
+	if tc.err != nil {
+		return
+	}
+	tc.getTime = time.Now()
+}
+
+func NewTimeCache[T any](expire time.Duration, getter func() (T, error)) *timeCache[T] {
 	tc := timeCache[T]{
 		expire: expire,
 		getter: getter,
